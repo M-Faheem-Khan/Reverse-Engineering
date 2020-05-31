1510652e3a968e855ba736beb13127c273ecc66f
M-Faheem-Khan <faheem5948@gmail.com>
2020-05-31 03:13:51 -0400

    added comments + header to level_1.c

--- src/level_1.c
+++ src/level_1.c
@@ -1,6 +1,10 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+// Author: Muhammad F. Khan
+// Github: https://github.com/M-Faheem-Khan/Reverse-Engineering
+
+// Libraries
+#include <stdlib.h> // Standard Library
+#include <stdio.h>  // Standard Input/Output
+#include <string.h> // String Library for strcmp() function
 
 
 // main function
@@ -9,10 +13,15 @@ int main(void) {
     char flag[] = "RevEng{Flag_1_easy_peasy}"; // flag variable
     char password[100]; // password variable
     
+    // Asking user for password
     printf("Please Enter the password to get the challenge flag!\n");
     printf("Password: ");
     fgets(password, 100, stdin); // reading from standard input
     
+    // Comparing passwords
+    // strcmp(): string compare(string1, string2)
+    // Returns: 0 if strings match
+
     if (strcmp("Password100", password) == 0) {
         printf("Flag: %s\n", flag);
     } else {
